0287aed6077398f227089f13e671e7f962ba027f
Dominik Haeusser <haeusser-dominik@web.de>
2021-04-20 20:40:58 +0200

    make style.css versionnumber dynamic

--- front-page.php
+++ front-page.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alles Theurer</title>
+    <!-- Load these Stylesheets dynamic with the wp_head-function -->
     <!-- Bootstrap CSS -->
-    <!-- Font Awesome -->
     <!-- Custom CSS -->
+    <!-- Font Awesome -->
     <?php wp_head(); ?>
 </head>
 <body>
--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
 
     function piano_theme_register_styles(){
-        wp_enqueue_style('piano_theme-bootstrap', get_template_directory_uri()."/bootstrap.css", array(), '1.0', 'all');
-        wp_enqueue_style('piano_theme-font-awesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css", array(), '1.0', 'all');
-        wp_enqueue_style('piano_theme-style', get_template_directory_uri()."/style.css", array(), '1.0', 'all');
+        $version = wp_get_theme()->get( 'Version' );
+        wp_enqueue_style('piano_theme-bootstrap', get_template_directory_uri()."/bootstrap.css", array(), '5.0.0-beta3', 'all');
+        wp_enqueue_style('piano_theme-style', get_template_directory_uri()."/style.css", array(), $version, 'all');
+        wp_enqueue_style('piano_theme-font-awesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css", array(), '5.15.3', 'all');
     }
 
     add_action( 'wp_enqueue_scripts', 'piano_theme_register_styles')
